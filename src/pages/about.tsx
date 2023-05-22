c51a27fee9e6252177ebacad3a33b0540be0f5d3
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-05-22 13:43:44 +0100

    test: test page and api

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function About() {
+    return <>
+        <Head>
+            <title>Next.js demo</title>
+        </Head>
+        <main>
+            About page
+        </main>
+    </>
+}
