74f89ceadad8e0f97f0358fd30e031a53fe8f94d
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-05-23 12:41:35 +0100

    test: test page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,9 +4,12 @@ export default function About() {
     return <>
         <Head>
             <title>Next.js demo</title>
+            {/* <meta name="description" content="question"/> */}
+            <meta name="viewport" content="width=deivce-width, initial-scale=1" />
+            {/* <link rel="icon" ref="/favicon.ico" /> */}
         </Head>
         <main>
-            About page
+            <h1>About Page</h1>
         </main>
     </>
 }
