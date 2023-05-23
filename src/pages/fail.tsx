17edfdf8da9c1f00ba21dffd79d87b2d41f3f224
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-05-23 12:38:54 +0100

    feat: question answer submission response

---
+++ src/pages/fail.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Fail() {
+    return <>
+        <Head>
+            <title>Next.js demo</title>
+            {/* <meta name="description" content="question"/> */}
+            <meta name="viewport" content="width=deivce-width, initial-scale=1" />
+            {/* <link rel="icon" ref="/favicon.ico" /> */}
+        </Head>
+        <main>
+            <h1>Fail Page</h1>
+        </main>
+    </>
+}
